18b5b7501728e6698321d0c097248bd93b961e4a
CJ <chriscjfern@hotmail.com>
2020-11-15 15:51:10 -0500

    Finishing touches! YAY!

--- Main.cpp
+++ Main.cpp
@@ -15,9 +15,7 @@ private:
 
 public:
 	void insertEdge(string from, string to);
-	bool isEdge(string from, string to);
 	vector<int> getOutDegreeVec();
-	vector<string> getAdjacent(string vertex);
 	void printGraph();
 	void PageRank(int n);
 };
@@ -45,19 +43,10 @@ void AdjacencyList::PageRank(int n) {
 				double sum = 0;
 				if (j == n - 1)
 					cout << it->first << " ";
-				//cout << getOutDegree(it->first);
-				//if (graph[it->second].size() != 0)
-					//cout << " ";
 				for (int i = 0; i < graph[it->second].size(); i++) {
-
 					sum += (rVec.at(index[graph[it->second].at(i)]) * (1.0 / outDegree.at(index[graph[it->second].at(i)])));
 					double one = rVec.at(index[graph[it->second].at(i)]);
 					double two = (1.0 / outDegree.at(index[graph[it->second].at(i)]));
-					//cout << rVec.at(index[graph[it->second].at(i)]) << " " << (1.0 / getOutDegree(graph[it->second].at(i))) << endl;
-					//cout << sum << endl;
-					//cout << graph[it->second].at(i);
-					//if (i != graph[it->second].size() - 1) // get rid of whitespace at end
-						//cout << " ";
 				}
 				rankVec.at(it->second) = sum;
 				if (j == n - 1) {
@@ -68,7 +57,6 @@ void AdjacencyList::PageRank(int n) {
 			}
 		}
 	}
-
 }
 
 void AdjacencyList::insertEdge(string from, string to) { // from lecture
@@ -81,15 +69,7 @@ void AdjacencyList::insertEdge(string from, string to) { // from lecture
 		graph[index[from]] = {};
 }
 
-bool AdjacencyList::isEdge(string from, string to) {
-	if (graph.find(index[from]) == graph.end() || graph.find(index[to]) == graph.end()) // Check if vertices exist
-		return false;
-	for (int i = 0; i < graph[index[from]].size(); i++) {
-		if (graph[index[from]].at(i) == to)
-			return true;
-	}
-	return false;
-}
+
 vector<int> AdjacencyList::getOutDegreeVec() {
 	vector<int> outDegree(j, 0);
 	for (auto it = index.begin(); it != index.end(); it++) {
@@ -97,23 +77,10 @@ vector<int> AdjacencyList::getOutDegreeVec() {
 			outDegree.at(index[graph[it->second].at(i)])++;
 		}
 	}
-	/*
-	for (int i = 0; i < outDegree.size(); i++) {
-		cout << outDegree.at(i) << " ";
-	}
-	*/
 	return outDegree;
 }
-vector<string> AdjacencyList::getAdjacent(string vertex) {
-	vector<string> adjacents = {};
-	if (graph.find(index[vertex]) == graph.end()) // does vertex exist?
-		return adjacents;
-	for (int i = 0; i < graph[index[vertex]].size(); i++) {
-		adjacents.push_back(graph[index[vertex]].at(i));
 
-	}
-	return adjacents;
-}
+/* FOR DEBUGGING! 
 void AdjacencyList::printGraph() {
 	for (auto it = index.begin(); it != index.end(); it++) {
 		cout << it->first << " ";
@@ -127,24 +94,21 @@ void AdjacencyList::printGraph() {
 		cout << endl;
 	}
 }
+*/
 
 int main()
 {
+	//while (true) { // For testing! :)
 	int no_of_lines, power_iterations;
 	std::string from, to;
-	AdjacencyList pageRank;
+	AdjacencyList webGraph;
 	std::cin >> no_of_lines;
 	std::cin >> power_iterations;
-	for (int i = 0; i < no_of_lines; i++)
-	{
+	for (int i = 0; i < no_of_lines; i++) {
 		std::cin >> from;
 		std::cin >> to;
-		pageRank.insertEdge(from, to);
-		// Do Something
+		webGraph.insertEdge(from, to);
 	}
-	cout << endl;
-	//pageRank.printGraph();
-	pageRank.PageRank(power_iterations);
-	//Create a graph object
-	//Created_Graph.PageRank(power_iterations);
+	webGraph.PageRank(power_iterations);
+	//}
 }
